158db3757034da12692cefe37e01f3ad9c0b1a4d
xiaoyu <794763733@qq.co>
2022-01-04 19:23:18 +0800

    1

---
+++ src/main/java/cn/machine/geek/utils/VersionUtil.java
@@ -0,0 +1,30 @@
+package cn.machine.geek.utils;
+
+/**
+ * @Author XiaoYu
+ * @Description 版本比较工具
+ * @Date 2021/11/2 5:54 下午
+ * @Email 794763733@qq.com
+ */
+public class VersionUtil {
+    public static int compareVersion(String version1, String version2) {
+        int n = version1.length(), m = version2.length();
+        int i = 0, j = 0;
+        while (i < n || j < m) {
+            int x = 0;
+            for (; i < n && version1.charAt(i) != '.'; ++i) {
+                x = x * 10 + version1.charAt(i) - '0';
+            }
+            ++i; // 跳过点号
+            int y = 0;
+            for (; j < m && version2.charAt(j) != '.'; ++j) {
+                y = y * 10 + version2.charAt(j) - '0';
+            }
+            ++j; // 跳过点号
+            if (x != y) {
+                return x > y ? 1 : -1;
+            }
+        }
+        return 0;
+    }
+}
