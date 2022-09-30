f67772872e372f63a2d55bdacf9fdac911412049
XiaoYu <yu.xiao@shopcider.com>
2022-09-30 16:39:46 +0800

    增加IP转换数字的工具

---
+++ src/main/java/cn/algo/yu/IpUtil.java
@@ -0,0 +1,16 @@
+package cn.algo.yu;
+
+public class IpUtil {
+    public static int ip2Int(String ipString) {
+        // 取 ip 的各段
+        String[] ipSlices = ipString.split("\\.");
+        int rs = 0;
+        for (int i = 0; i < ipSlices.length; i++) {
+            // 将 ip 的每一段解析为 int，并根据位置左移 8 位
+            int intSlice = Integer.parseInt(ipSlices[i]) << 8 * i;
+            // 或运算
+            rs = rs | intSlice;
+        }
+        return rs;
+    }
+}
